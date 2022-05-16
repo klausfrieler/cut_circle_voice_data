cf53375a702d46b0fb71d933c115fbb5c5d3ffa0
antonskrywer <das.schreiberle@gmail.com>
2022-05-16 12:08:31 +0200

    Annotation Function added

---
+++ scripts/annotate_note_numbers.py
@@ -0,0 +1,9 @@
+def annotate_note_numbers(piece, voice, first_measure, last_measure):
+    count = 1
+    piece = piece
+    voice = piece.parts[voice]
+    part = voice.measures(first_measure, last_measure)
+    for n in part.recurse().getElementsByClass("Note"):
+        if n.tie is None or n.tie.type == "start":
+            n.addLyric(str(count))
+            count += 1
